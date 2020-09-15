8cb9775640a863c48cdb700a79a8cfc68a5b6400
P <wolski@csag.uct.ac.za>
2020-09-15 07:15:39 +0200

    removing some old files

--- footer.php
+++
@@ -1,6 +0,0 @@
-<p>
-<span id=loginContainerFooter></span>
-<span class=clickable  onClick='pageinPopup(0.9,0.9,"termsofuse_contents","TRUE");'>Terms of use</span> &nbsp; | &nbsp;
-<span class=clickable onClick='pageinPopup(0.4,0.9,"disclaimer_contents","FALSE");'>Disclaimer</span>&nbsp; | &nbsp;
-<span class=clickable onClick='pageinPopup(0.4,0.9,"contactus_contents","FALSE");'>Contact Us</span>&nbsp; | &nbsp;
-</p>
